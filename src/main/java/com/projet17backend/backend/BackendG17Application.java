2ff4bfecff9bd542f791e19fdab4425b7d156d63
Boubacar Diallo <89472501+Boubacar167967@users.noreply.github.com>
2023-09-30 23:26:38 +0000

    Initial Commit

---
+++ src/main/java/com/projet17backend/backend/BackendG17Application.java
@@ -0,0 +1,13 @@
+package com.projet17backend.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendG17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendG17Application.class, args);
+	}
+
+}
