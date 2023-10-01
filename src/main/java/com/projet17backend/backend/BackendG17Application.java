cba0b9e1eb31671ae4f09f200cee432e285cb28d
Djibril Ba <djibril15.ba@ucad.edu.sn>
2023-10-01 18:40:35 +0000

    update

--- src/main/java/com/projet17backend/backend/BackendG17Application.java
+++ src/main/java/com/projet17backend/backend/BackendG17Application.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendG17Application {
 
 	public static void main(String[] args) {
+
+		System.out.println("Hello World");
 		SpringApplication.run(BackendG17Application.class, args);
 	}
 
